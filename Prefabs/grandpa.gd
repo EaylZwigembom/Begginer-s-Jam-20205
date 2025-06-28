31631e3228bbde997c8c3172f8635873974e9384
Eyal Zwigenbom <flemenko5@gmail.com>
2025-06-28 10:06:53 +0300

    ay 2

--- Prefabs/grandpa.gd
+++ Prefabs/grandpa.gd
@@ -1,4 +1,26 @@
-class_name Grandpa
+class_name gramps
 extends Node3D
 
-@export var grandpa_anim : AnimationPlayer
+@export var anim : AnimationPlayer
+@export var isBeatingStarted : bool
+@export var isKidDead : bool
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	isBeatingStarted = false
+	isKidDead = false
+	if anim:
+		GrampsPunch()  # or a real animation name
+	else:
+		push_error("grandpa_anim is null!")
+
+func GrampsPunch() -> void:
+	isBeatingStarted = true
+	anim.play("Punching2")
+	await anim.animation_finished
+	anim.play("Punching2")
+	await anim.animation_finished
+	isKidDead = true
+	await get_tree().create_timer(0.5).timeout
+	anim.play("Special Move")
+	await anim.animation_finished
+	anim.play("Celebrating")
--- Prefabs/kid.gd
+++ Prefabs/kid.gd
@@ -1,4 +1,27 @@
-class_name Kid
+class_name kid
 extends Node3D
 
-@export var child_anim : AnimationPlayer
+@onready var anim = $Model_And_Animations/AnimationPlayer
+@export var grandpa : gramps
+@onready var isDead
+@onready var isAnimationStarted
+@onready var isCalled = false;
+# Called when the node enters the scene tree for the first time.
+
+func ChildAnim() -> void:
+	if anim:
+		if isAnimationStarted:
+			while !isDead:
+				anim.play("Taking A Punch")
+	if isDead:
+		anim.play("Death")
+	else:
+		push_error("child_anim is null!")
+		
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	isDead = grandpa.isKidDead
+	isAnimationStarted = grandpa.isBeatingStarted
+	if isAnimationStarted and !isCalled:
+		ChildAnim()
+		isCalled = true

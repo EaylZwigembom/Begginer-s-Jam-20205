f766bf0bba27a0cd7be57800a7edb0c5bf7e2b06
Eyal Zwigenbom <flemenko5@gmail.com>
2025-06-29 13:00:59 +0300

    Day 3

---
+++ Scripts/gameManager.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+#@export var kidMonolog : Array[AudioEffect]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+	#kidMonolog[0].play()
+	#await kidMonolog[0].finished
+	#kidMonolog[1].play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
